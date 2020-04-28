81251b5332b6d514b254e9f56c629df3f02ddc31
ThePandoR <sasa9993@gmail.com>
2020-04-28 20:28:45 +0200

    init commit

---
+++ src/main/java/org/sep3/business/Sep3BusinessTierApplication.java
@@ -0,0 +1,13 @@
+package org.sep3.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep3BusinessTierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep3BusinessTierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sep3/business/Sep3BusinessTierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sep3.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep3BusinessTierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
